04379f47fe42c3c49cb6dd822f18de6ca57c858d
Jonas Emir <jonasemir00@gmail.com>
2023-01-23 22:46:36 -0300

    Implementação de conexão com o banco de dados, método adicionar e listar dados

---
+++ ControleDeContatos/DataBase/BancoContext.cs
@@ -0,0 +1,18 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.DataBase
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base (options)
+        {
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
---
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,30 @@
+﻿using ControleDeContatos.DataBase;
+using ControleDeContatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.Repositorio
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+        public ContatoRepositorio(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+        public List<ContatoModel> GetAll()
+        {
+            return _bancoContext.Contatos.ToList();
+        }
+
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            _bancoContext.Contatos.Add(contato);
+            _bancoContext.SaveChanges();
+
+            return contato;
+        }
+    }
+}
